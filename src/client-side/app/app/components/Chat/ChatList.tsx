11d759bcdebe711923e8986f198497c1bff178ae
jpfannku <jpfannku@student.42wolfsburg.de>
2023-06-13 14:59:31 +0200

    uhh

--- src/client-side/app/app/components/Chat/ChatList.tsx
+++ src/client-side/app/app/components/Chat/ChatList.tsx
@@ -6,7 +6,10 @@ const ChatList = ({chat}) => {
     <div>
 		  <h2>{chat.name}</h2>
 			{/* <p>{chat.id}</p> */}
-      {/* more beautiful things */}
+      {/* more beautiful things 
+        eg group or single chat
+        is user online 
+        idk */}
     </div>
   );
 };
