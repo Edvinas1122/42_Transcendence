c699d3d4ec76f8db6828b45f5f315ca2e108437d
Edvinas Momkus <emomkus@c2s7d23.42wolfsburg.de>
2023-07-07 18:14:29 +0200

    Profile picture hosting solution

--- src/client-side/app/next.config.js
+++ src/client-side/app/next.config.js
@@ -1,28 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	// async rewrites() {
-	// 	return [
-	// 		{
-	// 			source: '/avatar/',
-	// 			destination: '/avatar/',
-	// 		},
-	// 		{
-	// 			source: '/:any*',
-	// 			destination: '/',
-	// 		}
-	// 	]
-	// },
-	images: {
-		domains: ['localhost'],
-	},
-	experimental: {
-		serverActions: true,
-	},
-	serverRuntimeConfig: {
-		// Will only be available on the server side
-		backend_api_url: process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL,
-		sample: 'sample'
+	/** @type {import('next').NextConfig} */
+	const nextConfig = {
+		// async rewrites() {
+		// 	return [
+		// 		{
+		// 			source: '/avatar/',
+		// 			destination: '/avatar/',
+		// 		},
+		// 		{
+		// 			source: '/:any*',
+		// 			destination: '/',
+		// 		}
+		// 	]
+		// },
+		images: {
+			domains: ['localhost', `${process.env.NEXT_PUBLIC_HOST_NAME}`],
+		},
+		experimental: {
+			serverActions: true,
+		},
+		serverRuntimeConfig: {
+			// Will only be available on the server side
+			backend_api_url: process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL,
+			sample: 'sample'
+		}
 	}
-}
 
-module.exports = nextConfig
+	module.exports = nextConfig
