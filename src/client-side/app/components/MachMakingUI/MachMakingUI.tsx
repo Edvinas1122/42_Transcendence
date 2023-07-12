c517e0af7e0a3d2e5c65b8d80efe86bf29889151
Edvinas Momkus <emomkus@c2s7d23.42wolfsburg.de>
2023-07-12 23:13:26 +0200

    styling of game start

--- src/client-side/app/components/MachMakingUI/MachMakingUI.tsx
+++ src/client-side/app/components/MachMakingUI/MachMakingUI.tsx
@@ -24,9 +24,12 @@ const MachMakingUI: React.FC = () => {
 					/>
 				</>) : 
 				(<>
-					<h2>Live Que View</h2>
-					<LiveOnline/>
-					<InviteUserInterface/>
+					<div className="SelectGame">
+
+						<h2>Live Que View</h2>
+						<LiveOnline/>
+						<InviteUserInterface/>
+					</div>
 				</>)
 			}
 		</div>
